10bc1f848657e9fde181b29a5e4c80c3fd04469f
UdayKeerthi628 <udayuday0981@gmail.com>
2025-09-04 20:41:54 +0530

    Update App.jsx

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'https://51.21.105.226:9090/springapp1';
+const BASE_URL = 'https://54.89.37.222:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
